406c0e779dbaa2dabcc1bbb538ee89ed9536ebb7
raghavs6 <raghavsent@gmail.com>
2025-10-24 13:06:30 -0500

    Added bars visualization

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ const audio = document.getElementById("audio");
 const playBtn = document.getElementById("playButton");
 const pauseBtn = document.getElementById("puaseButton");
 const canvas = document.getElementById("visualizer");
+const canvasContext = canvas.getContext("2d");
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+
 
 console.log("audioFile:", audioFile);
 console.log("audio:", audio);
@@ -38,6 +43,7 @@ playBtn.addEventListener("click", function() {
   audio.play()
     .then(function() {
       console.log("Audio is playing!");
+      draw();
     })
     .catch(function(error) {
       console.error("Error playing audio:", error);
@@ -73,3 +79,38 @@ function setupAudioContext() {
   
   console.log("Web Audio API setup complete!");
 }
+
+function draw (){
+
+requestAnimationFrame(draw);
+analyser.getByteFrequencyData(dataArray);
+
+canvasContext.fillStyle = "rgb(0, 0, 0)";
+canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+
+const barWidth = (WIDTH / bufferLength) * 2.5;
+let barHeight;
+let x = 0;
+
+for(let i = 0; i < bufferLength; i++){
+  barHeight = dataArray[i];
+  canvasContext.fillStyle = "rgb(" + (barHeight + 100) + ", 50, 50)";
+  canvasContext.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+  
+  x += barWidth + 1;
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
